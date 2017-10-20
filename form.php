9fac090e02cf2e4955c6db126d5376f85ec30477
NgRuiYong <ngruiyong@hotmail.com>
2017-10-20 14:04:36 +0800

    Inserted text area and Jquery for baby names done

--- form.php
+++ form.php
@@ -2,6 +2,8 @@
 <html>
 <head>
   <title>A day with Pixel Onion</title>
+  <script src="assets/js/jquery.js"></script>
+  <script src="assets/js/script.js"></script>
 </head>
 <body>
   <header>
@@ -10,9 +12,26 @@
   </header>
   <section>
     Form
+    <div class="container">
+      <textarea form="listOfBabyNames" id="namesTextArea" rows="6" cols="50"></textarea>
+      <input type="submit" id="sortBtn">
+    </div>
+  <table id="myTable">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Gender</th>
+        <th>ID</th>
+      </tr>
+    </thead>
+    <tbody>
+
+    </tbody>
+  </table>
   </section>
   <footer>
     Footer
   </footer>
 </body>
+
 </html>
