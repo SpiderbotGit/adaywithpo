4d1f81a16303a8c1a3c681140bdfc0ee068b959b
Gavin <15sic045x@sit.singaporetech.edu.sg>
2017-10-20 14:10:19 +0800

    Updated Index.php

--- index.php
+++ index.php
@@ -1,43 +1,44 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <title>A day with Pixel Onion</title>
-    <!-- Style CSS -->
-  <link rel="stylesheet" href="assets/css/style.css" type="text/css">
-</head>
-<body>
-  <header>
-  <div style="background-color:#90caf9;padding-left:5%;padding-top:5%;"><img src="assets/images/pixel-onion-logo.png"/></div>
-    <div><img src="assets/images/full-banner.png" width="100%"/></div>
-  </header>
-  <section>
-    Form
-  </section>
-  <footer>
-    <div style="width80%;padding-left:20%;padding-right:20%;background-color:#90caf9">
-    <table style="padding:20px;">
-    <tr>
-    <td><img src="assets/images/banner-baby.jpg"/></td>
-    <td><h1 style="color:#FFF">Searching for the perfect name for your baby? Whether you want something traditional or unusual. We can help you!</h1></td>
-    </tr>
-    </table>
-    </div>
-    <div style="width:80%;padding-left:30%;padding-right:30%;">
-    	<table width="50%">
-        <tr>
-        <td>
-        <span>You can find us at:</span>
-        <span>991C Alexandra Road,#01-13B, Singapore 119971 or you can email us at <a href="mailto:hello@pixelonion.com">hello@pixelonion.com</a>
-        </td>
-        <td>
-        <span>Join Our Community</span>
-        </td>
-        </tr>
-        </table>
-    </div>
-    <div style="text-align:center;margin-top:40px;margin-bottom:40px;">
-    &copy; 2017 Pixel Onion Pte Ltd. All Right Reserved
-    </div>
-  </footer>
-</body>
+    <head>
+        <title>A day with Pixel Onion</title>
+        <!-- Style CSS -->
+        <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+    </head>
+    <body>
+        <header>
+            <div style="background-color:#90caf9;padding-left:5%;padding-top:5%;"><img src="assets/images/pixel-onion-logo.png"/></div>
+            <div><img src="assets/images/full-banner.png" width="100%"/></div>
+        </header>
+        <section>
+            <?php include 'form.php'; ?>
+            Form
+        </section>
+        <footer>
+            <div style="width80%;padding-left:20%;padding-right:20%;background-color:#90caf9">
+                <table style="padding:20px;">
+                    <tr>
+                        <td><img src="assets/images/banner-baby.jpg"/></td>
+                        <td><h1 style="color:#FFF">Searching for the perfect name for your baby? Whether you want something traditional or unusual. We can help you!</h1></td>
+                    </tr>
+                </table>
+            </div>
+            <div style="width:60%;margin-left:20%;margin-right:20%;">
+                <table width="50%">
+                    <tr>
+                        <td>
+                            <span>You can find us at:</span>
+                            <span>991C Alexandra Road,#01-13B, Singapore 119971 or you can email us at <a href="mailto:hello@pixelonion.com">hello@pixelonion.com</a>
+                        </td>
+                        <td>
+                            <span>Join Our Community</span>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <div style="text-align:center;margin-top:40px;margin-bottom:40px;">
+                &copy; 2017 Pixel Onion Pte Ltd. All Right Reserved
+            </div>
+        </footer>
+    </body>
 </html>
