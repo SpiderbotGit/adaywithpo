febf1b2987a87afe96aad919aaf253a07bf9a9f0
Programmingisfun93 <ChernHwee.Low_2015@SIT.SingaporeTech.edu.sg>
2017-10-20 15:15:26 +0800

    Added the style for body
    Added the style for body

--- index.php
+++ index.php
@@ -5,12 +5,9 @@
   <link rel="stylesheet" href="/BabyCare2/assets/css/style.css" type="text/css"/>
   <link href="/BabyCare2/assets/css/bootstrap.css" rel='stylesheet' />
 </head >
-<body class="background">
-  <header></header>
-  <section>
-    Main content
-  </section>
-  <div class="container">
+<body class="gray">
+  <header class="background"></header>
+  <div class="container white">
       <div class="row">
         <table align="center">
          <tr class="border">
@@ -26,8 +23,25 @@
        </table>
       </div>
     </div>
-  <footer>
-    Footer
-  </footer>
+  <div class="container white">
+      <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-4">
+                <br>
+                <br>
+                <p>You can find us at: </p>
+                <br>
+                991C Alexandra Road, #01-13B,Singapore 119971       
+                or you can email us at hello@pixelonion.com
+            </div>
+             <div class="col-sm-2"></div>
+            <div class="col-sm-4">
+                <br>
+                <br>
+                <p>Join our social community</p>
+                <br>
+            </div>  
+      </div>
+  </div> 
 </body>
 </html>
