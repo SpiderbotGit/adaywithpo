d22d8d338cb95c15d93e792f84eab8914675c405
Programmingisfun93 <ChernHwee.Low_2015@SIT.SingaporeTech.edu.sg>
2017-10-20 14:25:16 +0800

    Added table
    Fix table

--- index.php
+++ index.php
@@ -3,18 +3,32 @@
 <head>
   <title>A day with Pixel Onion</title>
   <link rel="stylesheet" href="/BabyCare2/assets/css/style.css" type="text/css"/>
-
-</head>
-<body>
-  <header  class = "background">
-    <div class ="logo">Pixel Onion logo</div>
-    <div >Site logo</div>
-  </header>
+  <link href="/BabyCare2/assets/css/bootstrap.css" rel='stylesheet' />
+</head >
+<body class="background">
+  <header></header>
   <section>
     Main content
   </section>
+  <div class="container">
+      <div class="row">
+        <table>
+         <tr>
+           <th>Name</th>
+           <th>Gender</th>
+           <th>ID</th>
+         </tr>
+         <tr>
+           <td>Abel</td>
+           <td>Boy</td>
+           <td>6941</td>
+         </tr>
+       </table>
+      </div>
+
   <footer>
     Footer
+    <div></div>
   </footer>
 </body>
 </html>
